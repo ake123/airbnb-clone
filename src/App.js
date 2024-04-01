f85f0cfd68938329cbb8f72852a299bad972f9e1
ake123 <akeketema@yahoo.com>
2024-04-01 20:18:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import Cards from './components/Cards'
+
+function App() {
+  return (
+    <div className='container'>
+      <NavBar />
+      <Cards />
+    </div>
+    )
+}
+
+export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,28 @@
+import desert from "./images/desert.png"
+import ImageCard from "./ImageCard"
+import cardData from "./data"
+
+function Cards(){
+
+    const cards = cardData.map((item)=>{
+        return(
+        <ImageCard
+        {...item}
+        />)
+    });
+
+    console.log(cards)
+    return (
+        <div className="cards-container">
+
+            <h1>Plan Your Trips Here</h1>
+
+            <div className="card-photos-container">
+                {cards}
+            </div>
+
+        </div>
+    )
+}
+
+export default Cards
